1ccce15e9dcee1a2397e2c2137a3e228357aca73
RS-Milano <61418538+RS-Milano@users.noreply.github.com>
2021-07-07 21:44:43 +0300

    Update signals.py

--- Project/backend/signals.py
+++ Project/backend/signals.py
@@ -18,33 +18,3 @@ def new_post_notify(sender, instance, created, **kwargs):
             from_email='test.for.sm@yandex.ru',
             recipient_list = email_list
         )
-
-        # all_category = ["sport", "politics", "education", "culture"]
-        all_category = ["sport"]
-        for i in all_category:
-            email_list = []
-            posts_id = []
-            posts_list = ""
-
-
-            for j in AuthorCategory.objects.filter(category = Category.objects.get(category = i)):
-                if j.author.user.email not in email_list:
-                    email_list.append(j.author.user.email)
-            
-            for n in PostCategory.objects.filter(category = Category.objects.get(category = i)):
-                if n.post.create_data.timestamp() > (datetime.datetime.now().timestamp() - 604800):
-                    posts_id.append(n.post.id)
-            
-            for my_post in posts_id:
-                posts_list += f'\nhttp://127.0.0.1:8000/news/{my_post}'
-
-            print(email_list)
-            print(posts_id)
-            print(posts_list)
-            if posts_list:
-                send_mail(
-                    subject = f"Тew posts this week",
-                    message = posts_list,
-                    from_email='test.for.sm@yandex.ru',
-                    recipient_list = email_list
-                )
